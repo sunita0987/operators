c8d6003aba3e188435e01845d68acb59d2a9facc
sunita0987 <sunita0987546@gmail.com>
2025-03-08 14:12:52 +0530

    add arithmetic operators

---
+++ assets/js/arithmetic.js
@@ -0,0 +1,33 @@
+//addition operators//
+let a = 10;
+b = 20;
+console.log( a + b );
+//substration operators//
+let x = 50;
+y = 10;
+console.log( x - y);
+//multiplication operators//
+let c = 5;
+d = 4;
+console.log( c * d );
+//division operators//
+let e = 20;
+f = 4;
+console.log(e / f);
+//Remainder operators//
+let h = 10;
+i = 3;
+console.log(h % i);
+//Exponentiation operators//
+let g = 4;
+k = 5;
+console.log(g ** k);
+//1024//
+
+
+
+
+
+
+
+
