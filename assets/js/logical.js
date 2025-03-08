dcfe27fd189f09b2185e108ceb8389561f2877c4
sunita0987 <sunita0987546@gmail.com>
2025-03-08 14:41:43 +0530

    add logical operators

--- assets/js/logical.js
+++ assets/js/logical.js
@@ -1,4 +1,18 @@
+/**
+ * 
+ * logical operators
+ */
+// logical AND operator//
 let u = 3;
 console.log(( u < 10) && (10 >= u));
 console.log((u <= 4) && (2 == 4));
 console.log((u > 4) && (u == 4)); 
+
+//logical OR operators//
+let v = 10;
+console.log(( v < 20) ||( 20 >= v) );
+console.log(( v <= 5) || ( 5 == 4));
+console.log((v > 5) || (v == 20 ));
+
+
+
