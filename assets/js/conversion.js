0645b64ec663d77375b8d0ba8338cfe35bfa63ca
sunita0987 <sunita0987546@gmail.com>
2025-03-08 15:50:38 +0530

    add conversion

---
+++ assets/js/conversion.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * conversion 
+ */
+
+//implicit conversion//
+// number convert to string//
+let result = "5" + 4;
+console.log(result);
+
+//string to number//
+let result2 = "10" - 5;
+console.log(result2);
+
+
+//boolean to number//
+let result3 = false + 4;
+console.log(result3);
+
+/**
+ * 
+ * explicit conversion
+ */
+//string to number//
+let str ="17849823";
+let num = Number(str);
+console.log(num);
+
+
+
+
