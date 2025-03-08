702aef4c5689355db64afbd702e898b6d09ca017
sunita0987 <sunita0987546@gmail.com>
2025-03-08 15:05:05 +0530

    add comparision

---
+++ assets/js/comparision.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * comparision operators
+ */
+//equal to operator//
+console.log( 5 == 5);
+console.log( 10 == "10");
+
+//not equal to operator//
+console.log( 3 != 3);
+console.log( 10 != "10");
+console.log( 10 != 20);
+
+//greater than or equal to operator//
+console.log( 10 >= 2 );
+console.log(2 >= 10 );
+
+//less then or equal to//
+console.log( 2 <= 10);
+console.log(10 <= 3);
+
+
+
+
+
+
+
+
