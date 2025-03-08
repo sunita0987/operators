cd48129a7a942b989584602c17ed9539b7d568ce
sunita0987 <sunita0987546@gmail.com>
2025-03-08 14:32:43 +0530

    add assignment operators

---
+++ assets/js/assignment.js
@@ -0,0 +1,23 @@
+//assign value//
+let m = 10;
+console.log(m);
+
+//add then assign value//
+let q = 20;
+q += 2;
+console.log(q);
+
+//subtract then assign value//
+let s = 40;
+s -= 20;
+console.log(s);
+
+//divide then assign value//
+let n = 20;
+n /= 5;
+console.log(n);
+
+//multiply then assign value//
+let o = 30;
+o *= 4;
+console.log(o);
---
+++ assets/js/logical.js
@@ -0,0 +1,4 @@
+let u = 3;
+console.log(( u < 10) && (10 >= u));
+console.log((u <= 4) && (2 == 4));
+console.log((u > 4) && (u == 4)); 
